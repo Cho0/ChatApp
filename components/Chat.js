d072c158d6e4a181605a534af17448add7ae0d82
Cho0 <choojason7@gmail.com>
2022-02-14 23:10:49 -0800

    5.2 wip

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,40 @@
 import React from "react";
-import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet, Platform, KeyboardAvoidingView } from "react-native";
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidmount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
   render() {
+
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
 
@@ -10,14 +42,23 @@ export default class Chat extends React.Component {
     const { backDropColor } = this.props.route.params;
 
     return (
+
       <View
         style={{
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
           backgroundColor: backDropColor
-        }}>
-
+        }}
+      >
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
